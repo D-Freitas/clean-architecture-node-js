9b6bcdc26acc9f83f304add1485ff4b43ed1447e
d-freitas <davi.israel01@gmail.com>
2022-09-15 13:51:28 -0300

    feat: add fake data

---
+++ src/infra/data-sources/books.ts
@@ -0,0 +1,23 @@
+export const books = [{
+  bookInformation: {
+    name: 'Clean Coder',
+    subject: 'Clean coding',
+    description: 'writing clean code',
+    author: 'Uncle Bob',
+    price: 20.50,
+    shelf: {
+      id: 5,
+      column: 10,
+      row: 3
+    }
+  },
+  bookSupplier: {
+    corporateName: 'Clean Coders',
+    phoneNumber: 14128121432,
+    emailAddress: 'unclebob@gmail.com',
+    address: 'R. Clean coders US',
+    cep: 9112120,
+    cnpj: 19230191
+  },
+  quantityOfProducts: 10
+}]
